589333481437de8e59637cd8786f4cff364a4611
Efosa Oyegun <efosao@gmail.com>
2023-02-24 18:12:19 -0800

    restored files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  /** @type {import('tailwindcss').Config} */
+  content: ["./app/**/*.{ts,tsx,jsx,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
