03fb661eee8971a6fc6288ee6abb304106e40ece
Efosa Oyegun <efosao@gmail.com>
2023-02-28 09:09:19 -0800

    fix tailwind dev styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  /** @type {import('tailwindcss').Config} */
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
     extend: {},
